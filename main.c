828aa3602095b6af41bf6ab079b7affd3421f547
Dawid Sowa <dawidpawelsowa@gmail.com>
2018-03-03 13:53:10 +0100

    Lint code refactor

--- main.c
+++ main.c
@@ -21,29 +21,24 @@ int main(int argc, char *argv[]) {
 	FILE *f;
 
    	init(argc, argv);	
-
-    
-    	for (i = 0; i < IT; i++) {
-
+	
+	for (i = 0; i < IT; i++) {
 		puts("\033[H\033[J");
 		printActualState(t);	
 
 		for ( j = 0; j < X_DIM; j++ ) {
 			for ( k = 0; k < Y_DIM; k++ ) { 
-	    	    
-		        	if (t[j][k].state == 1)
-
-			    		t[j][k].newState = !shouldBeDead(j,k, t);
- 		
-				else if (t[j][k].state == 0) 
-			
-	 		    		t[j][k].newState = shouldBeAlive(j,k, t);
-	    	    	}
+				if (t[j][k].state == 1) {
+					t[j][k].newState = !shouldBeDead(j,k, t);
+				} else if (t[j][k].state == 0) {
+					t[j][k].newState = shouldBeAlive(j,k, t);
+				}
+			}
 	 	}		
 
 		moveToNextGeneration(t);
 		usleep(200*1000);    
-    	}
+	}
 
 	puts("\033[H\033[J");
 	printActualState(t);
@@ -53,9 +48,8 @@ int main(int argc, char *argv[]) {
 		saveToFile(f);
 	}
 	
-
-    	for(i=0;i<X_DIM;i++) free(t[i]);
-    	free(t);
+	for(i=0;i<X_DIM;i++) free(t[i]);
+	free(t);
 
 	return 0;
 }
